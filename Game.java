a0a3dc5b2fc5ccbe053aec8041dd44f613f818d2
Jordan Siaha <jordansiaha@email.arizona.edu>
2018-01-14 00:18:00 -0700

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,196 @@
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import javax.swing.Timer;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+
+public class Game extends JFrame {
+
+//	public static void main(String[] args) {
+//		new Game().setVisible(true);
+//	}
+
+	private DefaultStyledDocument d = new DefaultStyledDocument();
+	private StyleContext context = new StyleContext();
+	private Style style = context.addStyle("test", null);
+	private JTextPane textPane = new JTextPane(d);
+	private char nextChar;
+	private String text;
+	private int count = 0;
+
+	private int sec = 0;
+	private int min = 1;
+	private JLabel timeLabel;
+	DecimalFormat df = new DecimalFormat("00");
+	private int spaceCount = 0;
+	private double errorCount = 0;
+	
+	private String filename;
+
+	// Timer
+	private Timer timer = new Timer(1000, new TimerListener()); // 1 sec
+
+	private class TimerListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			if (sec % 60 == 0) {
+				min--;
+				sec = 59;
+			} else {
+				sec--;
+			}
+			timeLabel.setText(min + ":" + df.format(sec));
+			if (min == 0 && sec == 0){
+				DecimalFormat df = new DecimalFormat("0.00");
+				String wpm = "WPM: " + spaceCount;
+				double errorDiff = count - errorCount;
+				String error = "" + df.format(((errorDiff / count) * 100)) + "%";
+				JOptionPane.showMessageDialog(null, "Game Over!\n" + wpm + "\nAccuracy: " + error);
+				System.exit(0);
+			}
+		}
+
+	}
+
+	public Game(String filename) {
+		this.filename =filename;
+		timeLabel = new JLabel(min + ":" + df.format(sec));
+		timeLabel.setFont(new Font("Time New Roman", 1, 20));
+		timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		Scanner s;
+		try {
+			s = new Scanner(new File(filename));
+			StringBuilder sb = new StringBuilder();
+			while (s.hasNextLine()) {
+				String str = s.nextLine();
+				sb.append(str);
+			}
+			text = sb.toString();
+			s.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		setup();
+	}
+
+	private void setup() {
+		this.setTitle("SpeedTyper");
+		this.setSize(new Dimension(800, 600));
+
+		textPane.addKeyListener(new KeyboardListener());
+
+		textPane.setFont(new Font("Time New Roman", 1, 20));
+		textPane.setAutoscrolls(true);
+		textPane.setText(text);
+
+		JScrollPane jsp = new JScrollPane(textPane);
+		// str = textPane.getText();
+
+		textPane.select(count, count + 1);
+
+		textPane.setEditable(false);
+		this.add(jsp, BorderLayout.CENTER);
+		this.add(timeLabel, BorderLayout.NORTH);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);this.setVisible(true);
+	}
+
+	private class KeyboardListener implements KeyListener {
+
+		@Override
+		public void keyPressed(KeyEvent key) {
+
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void keyTyped(KeyEvent key) {
+			timer.start();
+			System.out.print(key.getKeyChar());
+			if (key.getKeyChar() == '\b') {
+				count--;
+				if (count < 0) {
+					count = 0;
+				}
+				nextChar = text.charAt(count);
+				StyleConstants.setForeground(style, Color.BLACK);
+				if (nextChar == ' ') {
+					spaceCount--;
+					StyleConstants.setBackground(style, Color.WHITE);
+				}
+				try {
+					d.replace(count, 1, "" + nextChar, style);
+				} catch (BadLocationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				textPane.select(count, count + 1);
+			} else {
+				if (count > text.length() - 1) {
+					count = text.length() - 1;
+				}
+				nextChar = text.charAt(count);
+				if (key.getKeyChar() == nextChar) {
+					StyleConstants.setForeground(style, Color.GREEN);
+					try {
+						d.replace(count, 1, "" + nextChar, style);
+					} catch (BadLocationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} else {
+					errorCount++;
+					StyleConstants.setForeground(style, Color.RED);
+					if (nextChar == ' ') {
+						StyleConstants.setBackground(style, Color.RED);
+					} else {
+						StyleConstants.setBackground(style, Color.WHITE);
+					}
+					try {
+						d.replace(count, 1, "" + nextChar, style);
+					} catch (BadLocationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+
+				if (nextChar == ' ') {
+					spaceCount++;
+				}
+				count++;
+
+				textPane.select(count, count + 1);
+			}
+		}
+
+	}
+
+}
---
+++ TypeHackGUI.java
@@ -0,0 +1,188 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class TypeHackGUI extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	Stage currentStage;
+	Button singlePlayer;
+	Button multiPlayer;
+	Button back;
+	Button back2;
+	Scene mainMenuScene;
+	Scene singlePlayerScene;
+	Scene multiPlayerScene;
+	GridPane mainMenu;
+	BorderPane singlePlayerMenu;
+	GridPane multiPlayerMenu;
+	Label title;
+	Button b1 = new Button("Random");
+	Button b2 = new Button("A Christmas Carol");
+	Button b3 = new Button("A Resumed Identity");
+	Button b4 = new Button("Holmes' Final Problem");
+	Button b5 = new Button("The Idiots");
+	DropShadow borderGlow = new DropShadow();
+
+	final String FONT_NAME = "Georgia";
+	static final int X = 800;
+	static final int Y = 600;
+
+	@Override
+	public void start(Stage primaryStage) {
+		currentStage = primaryStage;
+		primaryStage.setTitle("TypeHack");
+
+		singlePlayer = new Button("Single Player");
+		multiPlayer = new Button("MultiPlayer");
+		back = new Button("Return to Main Menu");
+		back2 = new Button("Return to Main Menu");
+		title = new Label("TypeHack");
+		singlePlayer.setOnAction(e -> ButtonClicked(e));
+		multiPlayer.setOnAction(e -> ButtonClicked(e));
+		back.setOnAction(e -> ButtonClicked(e));
+		back2.setOnAction(e -> ButtonClicked(e));
+		b1.setOnAction(e -> ButtonClicked(e));
+		b2.setOnAction(e -> ButtonClicked(e));
+		b3.setOnAction(e -> ButtonClicked(e));
+		b4.setOnAction(e -> ButtonClicked(e));
+		b5.setOnAction(e -> ButtonClicked(e));
+
+		/*
+		 * Title 'TypeHack'
+		 */
+		title.setFont(Font.font(FONT_NAME, FontWeight.NORMAL, 30));
+		title.setTextFill(Color.WHEAT);
+
+		/*
+		 * Main Menu
+		 */
+		mainMenu = new GridPane();
+		// mainMenu.setPrefSize(950, 650);
+		mainMenu.setHgap(10);
+		mainMenu.setVgap(8);
+		mainMenu.setAlignment(Pos.CENTER);
+
+		mainMenu.setPadding(new Insets(10, 10, 10, 10));
+
+		mainMenuScene = new Scene(mainMenu, 400, 300);
+		mainMenuScene.getStylesheets().add("styles.css");
+		mainMenu.add(title, 0, 1);
+		mainMenu.setStyle("-fx-background-color: #061314;");
+		singlePlayer.getStyleClass().add("glass-wheat");
+		multiPlayer.getStyleClass().add("glass-wheat");
+		mainMenu.add(singlePlayer, 0, 0);
+		mainMenu.add(multiPlayer, 1, 0);
+		GridPane.setHalignment(singlePlayer, HPos.LEFT);
+		GridPane.setHalignment(multiPlayer, HPos.RIGHT);
+		GridPane.setHalignment(title, HPos.CENTER);
+
+		GridPane.setConstraints(title, 5, 1);
+
+		/*
+		 * Single Player Menu
+		 */
+
+		singlePlayerMenu = new BorderPane();
+		// singlePlayerMenu.setPrefSize(950, 650);
+		singlePlayerScene = new Scene(singlePlayerMenu, X, Y);
+		singlePlayerScene.getStylesheets().add("styles.css");
+		singlePlayerMenu.setPadding(new Insets(20));
+		singlePlayerMenu.setStyle("-fx-background-color: #061314;");
+		Button start = new Button("               Start              ");
+		start.getStyleClass().add("glass-wheat");
+		back.getStyleClass().add("glass-wheat");
+		HBox bottomButtons = new HBox(470);
+		bottomButtons.getChildren().addAll(back, start);
+		bottomButtons.setPadding(new Insets(10));
+
+		singlePlayerMenu.setBottom(bottomButtons);
+
+		VBox buttons = new VBox(5);
+		buttons.getStylesheets().add("styles.css");
+		Label vLabel = new Label("Select a Text");
+
+		borderGlow.setColor(Color.RED);
+		borderGlow.setOffsetX(0f);
+		borderGlow.setOffsetY(0f);
+		borderGlow.setHeight(70);
+
+		b1.getStyleClass().add("dark-blue");
+		b2.getStyleClass().add("dark-blue");
+		b3.getStyleClass().add("dark-blue");
+		b4.getStyleClass().add("dark-blue");
+		b5.getStyleClass().add("dark-blue");
+		vLabel.setFont(Font.font(FONT_NAME, FontWeight.NORMAL, 50));
+		vLabel.setTextFill(Color.WHEAT);
+		buttons.getChildren().addAll(vLabel, b1, b2, b3, b4, b5);
+		buttons.setAlignment(Pos.CENTER);
+
+		singlePlayerMenu.setCenter(buttons);
+
+		/*
+		 * MultiPlayer Menu
+		 */
+		multiPlayerMenu = new GridPane();
+		// multiPlayerMenu.setPrefSize(950, 650);
+		multiPlayerScene = new Scene(multiPlayerMenu, X, Y);
+		multiPlayerScene.getStylesheets().add("styles.css");
+		multiPlayerMenu.add(back2, 0, 0, 1, 1);
+		multiPlayerMenu.setStyle("-fx-background-color: #061314;");
+
+		primaryStage.setScene(mainMenuScene);
+		primaryStage.show();
+
+	}
+
+	public void ButtonClicked(ActionEvent e) {
+		if (e.getSource() == back || e.getSource() == back2)
+			currentStage.setScene(mainMenuScene);
+		else if (e.getSource() == singlePlayer) {
+			currentStage.setScene(singlePlayerScene);
+		} else if (e.getSource() == b1) {
+			Game game = new Game("Random");
+
+		} else if (e.getSource() == b2) {
+			Game game = new Game("A_Christmas_Carol");
+
+		} else if (e.getSource() == b3) {
+			Game game = new Game("A_Resumed_Identity");
+
+		} else if (e.getSource() == b4) {
+			Game game = new Game("Holmes_Final_Problem");
+
+		} else if (e.getSource() == b5) {
+			Game game = new Game("The_Idiots");
+
+		} else {
+			currentStage.setScene(multiPlayerScene);
+		}
+	}
+
+}
